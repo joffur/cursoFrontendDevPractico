ae46cf0f470910cad0c3b1abdd3bdd88be833483
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-02-03 05:21:51 -0500

    Finalizando la interactividad de la navbar con los menus disponibles

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
+//TO-DO pendiente el cierre de los menus cuando se de click en la seccion main de la página
+// body.addEventListener('click', closeMenus);
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
+
+function closeMenus() {
+  aside.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('slide-right');
+}
 
 function toggleDesktopMenu() {
+  aside.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
+  aside.classList.add('inactive');
   mobileMenu.classList.toggle('slide-right');
+}
+function toggleCartAside() {
+  mobileMenu.classList.add('slide-right');
+  desktopMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');
 }
